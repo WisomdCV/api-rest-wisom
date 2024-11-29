4a66c866a582fd82ffe19bfe466ca9569abe6060
WisomDCV <wilsondcv711@gmail.com>
2024-11-29 17:12:26 -0500

    Initial commit

---
+++ src/test/java/com/wisomdcv/apirestwisom/ApiRestWisomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisomdcv.apirestwisom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWisomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
